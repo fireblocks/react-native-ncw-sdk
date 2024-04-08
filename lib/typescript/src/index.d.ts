fc1641c725d57f826ee98399b0d62e64ac7cc4a3
Alon Bar <abar@fireblocks.com>
2024-04-08 11:05:55 +0300

    feat: adding git ignore lib

--- lib/typescript/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Spec } from "./NativeNcwSdk";
-declare const NcwSdk: Spec;
-export { NcwSdk };
-export * from "./interfaces";
-export * from "./types";
-export { FireblocksNCWFactory } from "./FireblocksNCWFactory";
-//# sourceMappingURL=index.d.ts.map
